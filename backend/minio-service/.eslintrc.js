868d5c6553f3b64131218177f1f735086b56e0b4
lukas spatschil <51056202+lukaspatschil@users.noreply.github.com>
2021-11-02 17:38:09 +0100

    More change?

--- backend/minio-service/.eslintrc.js
+++ backend/minio-service/.eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: 'tsconfig.json',
+    project: 'backend/minio-service/tsconfig.json',
     sourceType: 'module',
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
