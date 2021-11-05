bfc999fbc73cbda81703046eb788a25e6b17721a
SimonHofbauer <e11701818@student.tuwien.ac.at>
2021-11-05 18:11:53 +0100

    add swagger dependencies

--- backend/gateway-service/src/main.ts
+++ backend/gateway-service/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+      .setTitle('Federated Storage Infrastructure for IoT Sensor Data')
+      .setDescription('The federated Storage Infrastructure for IoT Sensor Data API description')
+      .setVersion('1.0')
+      .addTag('storage')
+      .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
