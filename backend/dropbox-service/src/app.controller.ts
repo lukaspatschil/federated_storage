b6f3a8744cc325a0ec5331739ccc34fec89577ed
SimonHofbauer <e11701818@student.tuwien.ac.at>
2021-11-09 14:29:27 +0100

    install npm dropbox, fix proto file for dropbox service

--- backend/dropbox-service/src/app.controller.ts
+++ backend/dropbox-service/src/app.controller.ts
@@ -1,29 +1,34 @@
 import { Metadata, ServerUnaryCall } from '@grpc/grpc-js';
 import { Controller } from '@nestjs/common';
 import { GrpcMethod } from '@nestjs/microservices';
+import DropboxConfig from '../dropbox-config.json';
+import { Dropbox } from 'dropbox';
 
 type PictureById = {
-  id: number;
+  id: string;
 };
 
-type Picture = {
-  id: number;
+export type Picture = {
+  id: string;
   name: string;
+  data: string;
 };
 
 @Controller()
 export class AppController {
   @GrpcMethod('DropboxService', 'FindOne')
-  findOne(
-    data: PictureById,
-    metadata: Metadata,
-    call: ServerUnaryCall<any, any>,
-  ): Picture {
+  findOne(data: PictureById): Picture {
     const items = [
-      { id: 1, name: 'John' },
-      { id: 2, name: 'Doe' },
+      { id: '1', name: 'John', data: 'Base64' },
+      { id: '2', name: 'Doe', data: 'Base64' },
     ];
 
     return items.find(({ id }) => id === data.id);
+  }
+
+  @GrpcMethod('DropboxService', 'CreateOne')
+  createOne(picture: Picture): Picture {
+    console.log(picture);
+    return null;
   }
 }
