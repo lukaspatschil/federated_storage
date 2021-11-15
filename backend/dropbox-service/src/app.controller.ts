a892581e51bd3867243f2ab509975473ce010b43
SimonHofbauer <e11701818@student.tuwien.ac.at>
2021-11-15 22:38:22 +0100

    implement first tries of dropbox

--- backend/dropbox-service/src/app.controller.ts
+++ backend/dropbox-service/src/app.controller.ts
@@ -2,7 +2,7 @@ import { Metadata, ServerUnaryCall } from '@grpc/grpc-js';
 import { Controller } from '@nestjs/common';
 import { GrpcMethod } from '@nestjs/microservices';
 import DropboxConfig from '../dropbox-config.json';
-import { Dropbox } from 'dropbox';
+import { Dropbox, files } from 'dropbox';
 
 type PictureById = {
   id: string;
@@ -18,6 +18,7 @@ export type Picture = {
 export class AppController {
   @GrpcMethod('DropboxService', 'FindOne')
   findOne(data: PictureById): Picture {
+    console.log('FindOne' + data);
     const items = [
       { id: '1', name: 'John', data: 'Base64' },
       { id: '2', name: 'Doe', data: 'Base64' },
@@ -27,8 +28,22 @@ export class AppController {
   }
 
   @GrpcMethod('DropboxService', 'CreateOne')
-  createOne(picture: Picture): Picture {
-    console.log(picture);
-    return null;
+  createOne(picture: Picture) {
+    console.log('CreateOne: ' + JSON.stringify(picture));
+    const dbx = this.login();
+    dbx
+      .filesUpload({ path: picture.id, contents: picture.data })
+      .then((response: any) => {
+        console.log(response);
+      })
+      .catch((uploadErr: Error) => {
+        console.log(uploadErr);
+      });
+  }
+
+  login(): Dropbox {
+    console.log('login function called');
+    const dbx = new Dropbox({ accessToken: DropboxConfig.accessToken });
+    return dbx;
   }
 }
--- backend/dropbox-service/src/main.ts
+++ backend/dropbox-service/src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
       transport: Transport.GRPC,
       options: {
         package: 'dropbox',
-        protoPath: join(__dirname, '../../proto/dropbox.proto'),
+        protoPath: join(__dirname, '../../proto/picture.proto'),
         url: 'dropbox-service:5000',
       },
     },
