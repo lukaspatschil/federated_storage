33652ab8d33915ccc55db0cfe076f66f39dd753d
lukas spatschil <51056202+lukaspatschil@users.noreply.github.com>
2021-11-18 18:54:38 +0100

    Update file names

--- backend/logger-service/src/app.service.ts
+++ backend/logger-service/src/app.service.ts
@@ -3,21 +3,30 @@ import { ConfigService } from '@nestjs/config';
 import { LogEntry } from '../../service-types/types';
 import fs from 'fs/promises';
 import { join } from 'path';
+import { Cron } from '@nestjs/schedule';
 
 @Injectable()
 export class AppService {
   private filePath: string;
 
   constructor(private configService: ConfigService) {
-    this.filePath = join(
-      this.configService.get<string>('FILE_PATH') ?? '.',
-      `${new Date().getUTCDate()}.log`,
-    );
+    this.updateFilePath();
   }
 
   async writeToFile(logEntry: LogEntry) {
-    const logMessage = `[${logEntry.serviceName}] -- ${logEntry.date}   ${logEntry.level}  ${logEntry.message}\n`;
+    const logMessage = `[${logEntry.serviceName}] -- ${logEntry.date}    ${logEntry.level}    ${logEntry.message}\n`;
 
     await fs.appendFile(this.filePath, logMessage);
+  }
+
+  @Cron('* * 12 * * *')
+  updateFilePath() {
+    let time = new Date().getTime();
+    time = time - (time % 86400000);
+
+    this.filePath = join(
+      this.configService.get<string>('FILE_PATH') ?? '.',
+      `${time}.log`,
+    );
   }
 }
